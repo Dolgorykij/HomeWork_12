84027168498d6e3be837c2270e7eefff4f9548e7
Dolgorykij <dolgo-rykij@mail.ru>
2024-05-25 02:01:29 +0300

    Done

--- src/Author.java
+++ src/Author.java
@@ -1,14 +1,35 @@
 public class Author {
+    private String authorFirstName;
+    private String authorSecondName;
     private String authorFullName;
 
-    public Author (String authorFullName) {
-        this.authorFullName = authorFullName;                   //Изначально вместо FullName было два параметра - firstName и secondName, но перемудрил в попытке сделать так, чтобы при вызове две перменные сливались, поэтому упростил.
+    public Author(String authorFirstName, String authorSecondName, String authorFullName) {
+        this.authorFirstName = authorFirstName;
+        this.authorSecondName = authorSecondName;
+        this.authorFullName = authorFirstName + authorSecondName;                   //Изначально вместо FullName было два параметра - firstName и secondName, но перемудрил в попытке сделать так, чтобы при вызове две перменные сливались, поэтому упростил.
     }
+
     public String getAuthorFullName() {
         return this.authorFullName;
     }
+
     public void setAuthorFullName(String authorFullName) {
         this.authorFullName = authorFullName;
+    }
+
+    public String getAuthorFirstName() {
+        return this.authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+    public String getAuthorSecondName(String authorSecondName) {
+        return this.authorSecondName;
+    }
+
+    public void setAuthorSecondName(String authorSecondName) {
+        this.authorSecondName = authorSecondName;
     }
 }
 //
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Author killanAr = new Author("Killan Arnold");
+        Author killanAr = new Author("Killan"," Arnold","");
         Book magica = new Book("Magica", 1999, killanAr);
-        Author kozlovskiDa = new Author("Danila Kozlovski");
+        Author kozlovskiDa = new Author("Danila", "Kozlovski","" );
         Book kamonRebyat = new Book("Kamon Rebyat", 2016, kozlovskiDa);
         kamonRebyat.setRelease(2022);
         System.out.println(kamonRebyat.getRelease());
+        System.out.println(killanAr.getAuthorFullName());
     }
 }
